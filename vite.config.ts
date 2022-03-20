e87d471fba35ddf1fdcc0a12f418616d03c0e1dd
Carson Stone <carson@landis.com>
2022-03-20 15:10:29 -0400

    add some configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ vue(), ],
+  plugins: [ vue(),
+    tsconfigPaths(), ],
+  server: { port: 5000, },
+  build : {
+    commonjsOptions: {
+      include: [ /lib-cjs/u, /node_modules/u, ],
+    },
+    outDir: "out",
+  },
 });
